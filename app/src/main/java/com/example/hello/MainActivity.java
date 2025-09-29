c8e13994b71b32d99e4828a7aa08accef74a7a26
HSH-11 <lyk1517@naver.com>
2025-09-29 22:24:16 +0900

    edit text,RandomNumber

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +13,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewRandomNumber;
+    private TextView textView;
+    EditText eText;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) { // 애플리케이션이 이전에 실행되었던 상태 전달
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main); // activity_main.xml 파일을 액티비티 화면으로
+        textViewRandomNumber = findViewById(R.id.textViewRandomNumber);
+        eText = findViewById(R.id.edittext);
+        textView = findViewById(R.id.textView);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,8 +36,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void onClicked(View v){
+    public void onClicked_Call(View v){
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1111-2222"));
         startActivity(intent);
+    }
+
+    public void onClicked_Text(View v){
+        String str = eText.getText().toString();
+        textView.setText(str);
+    }
+
+
+    public void generateRandomNumber(View view){
+        //난수 생성
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        // 텍스트 뷰에 난수 표시
+        textViewRandomNumber.setText("난수: "+randomNumber);
     }
 }
