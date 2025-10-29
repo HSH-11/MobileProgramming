f4d83422049312bd6cd2e7a5b838676ab47ac774
HSH-11 <lyk1517@naver.com>
2025-10-29 14:43:53 +0900

    SingleTouchEvent

---
+++ singletouch/src/main/java/com/example/singletouch/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.singletouch;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SingleTouchView(this,null));
+
+    }
+}
